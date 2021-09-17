bacb797ec23878e89fbce029319077adc3a49857
Sam Erickson <samericksn@gmail.com>
2021-09-17 19:45:11 +0000

    Add curly backets to variables in install script

--- init/install.sh
+++ init/install.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # TODO: unpack dotfiles as symbolic links in the home directory
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "$HOME/.profile"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "$HOME/.zprofile"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/bashrc" "$HOME/.bashrc"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/vimrc" "$HOME/.vim/vimrc"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/tmux.conf" "$HOME/.tmux.conf"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "${HOME}/.profile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "${HOME}/.zprofile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/bashrc" "${HOME}/.bashrc"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/vimrc" "${HOME}/.vim/vimrc"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/tmux.conf" "${HOME}/.tmux.conf"
 ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/zshrc" "${XDG_CONFIG:-$HOME/.config}/zsh/.profile"
 ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/wgetrc" "${XDG_CONFIG:-$HOME/.config}/wgetrc"

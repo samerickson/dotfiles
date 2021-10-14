d81ab24f6500cad0a0b7d5c4521e1cd446d9abb0
Sam Erickson <samericksn@gmail.com>
2021-10-14 01:54:56 +0000

    Fix path in install script

--- init/install.sh
+++ init/install.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 # TODO: unpack dotfiles as symbolic links in the home directory
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "${HOME}/.profile"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/profile" "${HOME}/.zprofile"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/bashrc" "${HOME}/.bashrc"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/vimrc" "${HOME}/.vim/vimrc"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/tmux.conf" "${HOME}/.tmux.conf"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/profile" "${HOME}/.profile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/profile" "${HOME}/.zprofile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/bashrc" "${HOME}/.bashrc"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/vimrc" "${HOME}/.vim/vimrc"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/tmux.conf" "${HOME}/.tmux.conf"
 
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/zsh"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/wget"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/zshrc" "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.profile"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles/}/src/wgetrc" "${XDG_CONFIG_HOME:-$HOME/.config}/wget/wgetrc"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/zshrc" "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.profile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/wgetrc" "${XDG_CONFIG_HOME:-$HOME/.config}/wget/wgetrc"

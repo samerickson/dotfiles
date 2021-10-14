e386ede388289b7bd184ac2c44495165062a7780
Samuel Brian Erickson (Sam) <erickssb@stumail.csci.viu.ca>
2021-10-13 21:36:06 -0700

    Add backup to install script

--- init/install.sh
+++ init/install.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
 
+# Backup files that will get over written
+# TODO: These files could be in a variety of places can be, I may want to try and detect this
+mkdir -p "${HOME}/.backup-dotfiles"
+[ -f "${HOME}/.profile" ] && cp "${HOME}/.profile"
+[ -f "${HOME}/.zprofile" ] && cp "${HOME}/.zprofile"
+[ -f "${HOME}/.bashrc" ] && cp "${HOME}/.bashrc"
+[ -f "${HOME}/.vim/vimrc" ] && cp "${HOME}/.vim/vimrc"
+[ -f "${HOME}/.tmux.conf" ] && cp "${HOME}/.tmux.conf"
+[ -f "${HOME}/." ] && cp "${HOME}/.profile"
+[ -f "${XDG_CONFIG_HOME:-$HOME/config}/zsh/.zshrc" ] && cp "${XDG_CONFIG_HOME:-$HOME/config}/zsh/.zshrc"
+[ -f "${XDG_CONFIG_HOME:-$HOME/config}/wget/wgetrc" ] && cp "${XDG_CONFIG_HOME:-$HOME/config}/zsh/.zshrc"
+
 # TODO: unpack dotfiles as symbolic links in the home directory
 ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/profile" "${HOME}/.profile"
 ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/profile" "${HOME}/.zprofile"
@@ -9,5 +21,5 @@ ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/tmux.conf" "${HOME}/.tmux.conf"
 
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/zsh"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/wget"
-ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/zshrc" "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.profile"
+ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/zshrc" "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.zshrc"
 ln -sfv "${DOTFILES:-$HOME/.dotfiles}/src/wgetrc" "${XDG_CONFIG_HOME:-$HOME/.config}/wget/wgetrc"

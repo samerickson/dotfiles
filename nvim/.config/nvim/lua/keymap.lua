95f5b0c58abeeed8a6303ac9597949a95fd545d8
Sam Erickson <samericksn@gmail.com>
2021-12-04 03:55:29 +0000

    Add neovim config files

---
+++ nvim/.config/nvim/lua/keymap.lua
@@ -0,0 +1,20 @@
+-- keymap.lua
+
+-- Map method to handle the different kinds of key maps
+local function map(mode, combo, mapping, opts)
+  local options = {noremap = true}
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, combo, mapping, options)
+end
+
+vim.g.mapleader = ',' -- change the <leader> key to be comma
+
+-- Keymappings
+map('i', 'jk', '<Esc>', {noremap=true}) -- Map jk to <Esc>
+map('n', '<leader>df', ':bn <CR>', {noremap=true}) -- Go to next buffer using <leader>df
+map('n', '<leader>fd', ':bp <CR>', {noremap=true}) -- Go to previous buffer using <leader>fd
+
+-- Quick open frequent files
+map('n', '<leader>ea', ':e ~/.config/shell/aliases <CR>')

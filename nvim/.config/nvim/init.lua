211e35e5c0a1d098be26e58daa0bcd000e9b85e1
Sam Erickson <sam@samerickson.dev>
2021-12-30 11:05:57 -0800

    Add highlight yanked text in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,3 +6,10 @@ require("keymap")
 require("keybr")
 require("opts")
 require("colors")
+
+vim.cmd [[
+	augroup highlight_yank
+		autocmd!
+		autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 100})
+	augroup END
+]]

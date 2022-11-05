2359dba44bf172864b1491a1bc619f34fd13529d
Sam Erickson <serickson@gmail.com>
2022-11-05 14:59:36 -0700

    Add windows nvim files

---
+++ AppData/Local/nvim/lua/keymap.lua
@@ -0,0 +1,47 @@
+-- keymap.lua
+
+local key = require("map")
+
+vim.g.mapleader = ',' -- change the <leader> key to be comma
+
+local opts = { noremap = true, silent = true }
+
+key.map('i', 'jk', '<Esc>', opts) -- Map jk to <Esc>
+
+-- BUFFERS
+key.map('n', '<leader>df', ':bn <CR>', opts)
+key.map('n', '<leader>fd', ':bp <CR>', opts)
+
+-- REGEX replace
+key.map('n', 'R', ':%s//g<Left><Left>', opts)
+
+-- Show/hide whitespace characters
+key.map('n', '<leader>l', ':set list!<CR>', opts)
+
+-- Show/hide line numbers
+key.map('n', '<leader>n', ':set number!<CR>', opts)
+key.map('n', '<leader>m', ':set relativenumber!<CR>', opts)
+
+-- Generate documentation
+key.map('n', '<leader>d', ':lua require("neogen").generate()<CR>', opts)
+
+-- Highlight lines over 80 characters
+key.map('n', '<leader>g', ':match Error /\\%>80c/<CR>', opts)
+key.map('n', '<leader>h', ':match Text /\\%>80c/<CR>',opts)
+
+-- Telescope
+key.map('n', '<C-p>', ':lua require("telescope.builtin").git_files()<CR>')
+key.map('i', '<C-p>', ':lua require("telescope.builtin").git_files()<CR>')
+key.map('n', '<C-o>', ':lua require("telescope.builtin").find_files()<CR>')
+key.map('i', '<C-o>', ':lua require("telescope.builtin").find_files()<CR>')
+
+-- LSP
+key.map('n', '<Leader>k', ':lua vim.lsp.buf.hover()<CR>', opts)
+key.map('n', '<F12>', ':lua vim.lsp.buf.definition()<CR>', opts)
+key.map('n', '<S-F12>', ':lua vim.lsp.buf.references()<CR>', opts)
+key.map('n', '<C-F12>', ':lua vim.lsp.buf.implementation()<CR>', opts)
+key.map('n', '<C-r><C-r>', ':lua vim.lsp.buf.rename()<CR>', opts)
+key.map('n', 'C-.>', ':lua vim.lsp.buf.code_action()<CR>', opts)
+key.map('n', '<C-n>', ':lua vim.diagnostic.goto_next()<CR>', opts)
+key.map('n', '<C-m>', ':lua vim.diagnostic.goto_prev()<CR>', opts)
+key.map('n', '<F8>', '[[<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>]]', opts)
---
+++ AppData/Local/nvim/lua/opts.lua
@@ -0,0 +1,23 @@
+-- opts.lua
+
+local tabSize = 4
+
+local set = vim.opt
+
+set.tabstop = tabSize
+set.shiftwidth = tabSize
+set.softtabstop = tabSize
+set.expandtab = true
+set.swapfile = false
+set.list = true
+
+set.scrolloff = 10
+set.numberwidth = 5
+
+-- Have gutter be used even if line number are not being displayed
+set.signcolumn = "yes"
+
+set.updatetime = 1000
+set.cmdheight = 1
+
+set.listchars = {tab='» ', extends='⟩', precedes='⟨', trail='•'}

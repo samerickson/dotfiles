91249bc722808d6b086cb5cd3803fd35b3e4dfa4
Sam Erickson <serickson@reliablecontrols.com>
2023-02-23 09:35:04 -0800

    Remove unused file

--- nvim/.config/nvim/lua/map.lua
+++
@@ -1,9 +0,0 @@
-return {
-	map = function(mode, combo, mapping, opts)
-		local options = {noremap = true}
-		if opts then
-			options = vim.tbl_extend('force', options, opts)
-		end
-		vim.api.nvim_set_keymap(mode, combo, mapping, options)
-	end
-}

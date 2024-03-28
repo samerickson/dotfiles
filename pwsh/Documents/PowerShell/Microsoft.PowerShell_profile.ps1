6518ab765eb94249d0bdc6de40623731b303fccf
Sam Erickson <erickson.samuel@outlook.com>
2024-03-27 19:48:18 -0700

    feat: add better ls colors for powershell

--- pwsh/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ pwsh/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,9 @@ Invoke-Expression (&starship init powershell)
 
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 
+# If this fails it can be installed with Install-Module PSColor
+Import-Module PSColor
+
 $aliases="$([Environment]::GetFolderPath("MyDocuments"))\PowerShell\Aliases.ps1"
 $private="$([Environment]::GetFolderPath("MyDocuments"))\PowerShell\Private.ps1"
 
@@ -14,3 +17,4 @@ if (Test-Path -Path $aliases) {
 if (Test-Path -Path $private) {
   . $private
 }
+

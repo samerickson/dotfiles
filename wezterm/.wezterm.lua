8afb0f1c472255e8f1d16042d0a5b114ab8b3c34
Sam Erickson <samericksn@gmail.com>
2024-12-22 13:50:04 -0800

    docs: leave comment in wezterm config for catppuccin color scheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,6 +12,15 @@ config.default_prog = { "pwsh.exe" }
 config.font = wezterm.font(font)
 config.font_size = 13
 
+-- function scheme_for_appearance(appearance)
+-- 	if appearance:find("Dark") then
+-- 		return "Catppuccin Mocha"
+-- 	else
+-- 		return "Catppuccin Latte"
+-- 	end
+-- end
+--
+-- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.font_size = 13
 config.cell_width = 0.9
